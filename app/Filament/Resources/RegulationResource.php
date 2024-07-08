4e5ba9d93889f4fc0419a80ebcc3fccdb9df4359
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-08 16:00:36 -0500

    Agregando el botón de ver

--- app/Filament/Resources/RegulationResource.php
+++ app/Filament/Resources/RegulationResource.php
@@ -73,6 +73,7 @@ class RegulationResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
