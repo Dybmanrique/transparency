79387a2e4144688dbb1cf76acc0adf2da85ce5b9
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-08 15:19:21 -0500

    Crud de información extra

---
+++ app/Filament/Resources/InformationResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\InformationResource\Pages;
+use App\Filament\Resources\InformationResource\RelationManagers;
+use App\Models\Information;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class InformationResource extends Resource
+{
+    protected static ?string $model = Information::class;
+
+    protected static ?string $navigationGroup = 'Información extra';
+    protected static ?string $navigationIcon = 'heroicon-o-information-circle';
+
+    protected static ?string $label = 'Información';
+    protected static ?string $pluralLabel = 'Información';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->label('Nombre')
+                    ->required()
+                    ->maxLength(255)
+                    ->columnSpanFull(),
+                Forms\Components\Textarea::make('description')
+                    ->label('Descripción')
+                    ->maxLength(600)
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\Toggle::make('is_active')
+                    ->default(true)
+                    ->label('Esta activo')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nombre')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->label('Descripcion')
+                    ->searchable()
+                    ->limit(60),
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean()
+                    ->label('Es activo'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListInformation::route('/'),
+            'create' => Pages\CreateInformation::route('/create'),
+            'edit' => Pages\EditInformation::route('/{record}/edit'),
+        ];
+    }
+}
