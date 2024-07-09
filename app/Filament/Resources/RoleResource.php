4b2c2d31dac6859383f62ca61dcca1c0b96cc187
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-09 12:46:29 -0500

    Creando el crud de roles

---
+++ app/Filament/Resources/RoleResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RoleResource\Pages;
+use App\Filament\Resources\RoleResource\RelationManagers;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Spatie\Permission\Models\Role as ModelsRole;
+
+class RoleResource extends Resource
+{
+    protected static ?string $model = ModelsRole::class;
+
+    protected static ?int $navigationSort = 7;
+    protected static ?string $navigationIcon = 'heroicon-o-key';
+    protected static ?string $navigationGroup = 'Sistema';
+    protected static ?string $label = 'Rol';
+    protected static ?string $pluralLabel = 'Roles';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->label('Nombre')
+                ->required()->unique(ignoreRecord: true)->maxLength(255)->columnSpanFull(),
+                Forms\Components\Select::make('permissions')->label('Permisos')
+                ->multiple()->relationship('permissions', 'name')->columnSpanFull()->preload()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->label('Nombre')->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageRoles::route('/'),
+        ];
+    }
+}
