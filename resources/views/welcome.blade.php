0197f7afaee7463f1801ac417bb2b82d35710956
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-15 11:37:11 -0500

    Agregando el titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,10 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Transparencia UNASAM</title>
   <link rel="stylesheet" href="{{asset('css/style.css')}}">
   @vite('resources/css/app.css')
 </head>
@@ -19,6 +21,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <section class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
                 <x-card title="Numeral 11 de la ley universitaria" description='Una descripción de la ley universitaria' />
+                <x-card title="Indicador-CBC" description='Una descripción del inidicador CBC' />
             </section>
         </div>
     </main>
