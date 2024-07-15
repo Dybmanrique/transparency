62634d524fcdbc45d10663d8ea8ad6fa9ee610dd
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-15 12:17:13 -0500

    convirtiendo en componente la imagen de fondo

---
+++ resources/views/components/back-image.blade.php
@@ -0,0 +1,8 @@
+<section class="hero">
+    <img src="{{ asset('img/university-campus.jpg') }}" alt="Campus Universitario"
+        class="w-full object-cover block mb-4" @if ($isComplete == "0") style="max-height: 200px" @endif>
+    <div class="hero-text p-5 lg:p-10">
+        <h1 class="font-bold text-xl md:text-4xl">TRANSPARENCIA UNASAM</h1>
+        <p class="uppercase text-sm md:text-xl">Una nueva Universidad para el Desarrollo</p>
+    </div>
+</section>
