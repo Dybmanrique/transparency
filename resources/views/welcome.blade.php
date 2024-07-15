602eabf4fd7ad81ca183591388bb136c1b428c6a
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-15 12:17:31 -0500

    remplazando por el componente de imagen de fondo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,14 +13,7 @@
 <body class="bg-gray-200">
     <x-header />
     <main>
-        <section class="hero">
-            <img src="{{ asset('img/university-campus.jpg') }}" alt="Campus Universitario"
-                class="w-full object-cover block mb-4">
-            <div class="hero-text p-5 md:p-10">
-                <h1 class="font-bold text-xl md:text-4xl">TRANSPARENCIA UNASAM</h1>
-                <p class="uppercase text-sm md:text-xl">Una nueva Universidad para el Desarrollo</p>
-            </div>
-        </section>
+        <x-back-image isComplete="1" />
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <section class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
                 <x-card title="Numeral 11 de la ley universitaria"
@@ -37,7 +30,7 @@
     </main>
     <x-footer />
     <x-social-networks />
-    <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/collapse.js"></script>
+    <script src="{{asset('js/scripts.js')}}"></script>
     <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
 </body>
 
