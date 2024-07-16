9f22fcaaea0add7f69342b040376ef6f30e0b037
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-16 16:42:51 -0500

    Agregando la url de redireccionamiento

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-white p-5 m-2 text-center border-l-4 border-blue-950 rounded-md transition-transform transform hover:scale-110 md:min-h-40"><!-- Card -->
-    <a href="https://unasam.edu.pe/misionvision">
+    <a href="{{$link}}">
         <h2 class="uppercase text-blue-950 mb-4 font-bold text-lg">{{$title}}</h2>
         <p>{{$description}}</p>
     </a>
