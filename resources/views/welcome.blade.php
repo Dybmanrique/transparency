15f6e57009f6054036ec784b01bb762b62eaca01
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-16 18:38:05 -0500

    agregando el link de los indicadores cbc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,14 +16,15 @@
         <x-back-image isComplete="1" />
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <section class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
+                
                 <x-card title="Numeral 11 de la ley universitaria"
-                    description='Una descripción de la ley universitaria' />
-                <x-card title="Indicador-CBC" description='Una descripción del inidicador CBC' />
+                    description='Una descripción de la ley universitaria' link="{{route('numerals')}}" />
+                <x-card title="Indicador-CBC" description='Una descripción del inidicador CBC' link="{{route('indicador_cbc')}}" />
                 @foreach ($informations as $information)
-                    <x-card title="{{$information->name}}" description='{{$information->description}}' />
+                    <x-card title="{{$information->name}}" description='{{$information->description}}' link='xd'/>
                 @endforeach
                 @foreach ($documents as $document)
-                    <x-card title="{{$document->name}}" description='{{$document->description}}' />
+                    <x-card title="{{$document->name}}" description='{{$document->description}}' link='xd'/>
                 @endforeach
             </section>
         </div>
