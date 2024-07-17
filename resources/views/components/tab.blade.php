a9c3923d8cfcc0a5436ffbdd1f543acfde1b5433
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-16 19:08:59 -0500

    Creando un componente para las tabs

---
+++ resources/views/components/tab.blade.php
@@ -0,0 +1,5 @@
+<a href="{{ $link }}">
+    <div class=" bg-white p-2 m-1 border-l-4 border-blue-950 rounded-md transition-transform transform hover:scale-105">
+        <h2 class="uppercase text-sm text-blue-950 font-bold">{{ $title }}</h2>
+    </div>
+</a>
