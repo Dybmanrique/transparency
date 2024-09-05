f56b76ed5aa1116257fe665418b30793f79c818a
Deyber Manrique <dbrmanrique@gmail.com>
2024-09-05 16:30:45 -0500

    Agregando el link a las redes sociales

--- resources/views/components/social-networks.blade.php
+++ resources/views/components/social-networks.blade.php
@@ -1,10 +1,10 @@
 <div class="fixed bottom-4 right-4 z-50">
     <input type="checkbox" id="btn-mas">
     <div class="redes">
-        <a href="#" class="fa-brands fa-facebook bg-blue-900 text-white"></a>
-        <a href="#" class="fa-brands fa-youtube bg-blue-900 text-white"></a>
-        <a href="#" class="fa-brands fa-twitter bg-blue-900 text-white"></a>
-        <a href="#" class="fa-brands fa-pinterest bg-blue-900 text-white"></a>
+        <a target="_blank" href="https://www.facebook.com/unasam.edu.pe" class="fa-brands fa-facebook bg-blue-900 text-white"></a>
+        <a target="_blank" href="https://www.youtube.com/channel/UCHUxOdDI4zrMgghSpTDxttw" class="fa-brands fa-youtube bg-blue-900 text-white"></a>
+        <a target="_blank" href="https://www.instagram.com/unasam.edu.pe/" class="fa-brands fa-instagram bg-blue-900 text-white"></a>
+        <a target="_blank" href="https://rtv.unasam.edu.pe/inicio" class="fa-solid fa-radio bg-blue-900 text-white"></a>
     </div>
     <div class="btn-mas">
         <label for="btn-mas" class="fa-solid fa-at bg-gray-600 text-white"></label>
